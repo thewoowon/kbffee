340906fdc7604fbff3efe2d26e6343977f79f350
thewoowon <thewoowon@naver.com>
2025-02-15 00:25:06 +0900

    feat: babel configure edited

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: `.env.${process.env.NODE_ENV || 'development'}`,
+        blacklist: null,
+        whitelist: null,
+        safe: true,
+        allowUndefined: true,
+      },
+    ],
+  ],
 };
