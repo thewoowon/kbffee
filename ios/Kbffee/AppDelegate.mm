933caccca58d2c63770f910264e79b1ee59f85a6
thewoowon <thewoowon@naver.com>
2025-02-16 15:04:14 +0900

    refactor: some settings added for using firestore options

--- ios/Kbffee/AppDelegate.mm
+++ ios/Kbffee/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <Firebase.h>  // ✅ FirebaseCore 추가 (import 방식)
 
 @implementation AppDelegate
 
@@ -10,6 +11,11 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
+  // ✅ Firebase 초기화 (중복 방지)
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
